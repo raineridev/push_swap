406c59793579d93369b9c84811d05af95c4226d3
Marcelo Raineri <raineridev@gmail.com>
2024-09-30 13:36:56 +0100

    add: 'sa' function

--- Moviments/swap.c
+++ Moviments/swap.c
@@ -4,16 +4,17 @@ void	sa(ps_lst **stack_a)
 {
 	if(!stack_a)
 		return ;
-
-
+	
+	ps_lst *tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	tmp->next = (*stack_a)->next;
+	(*stack_a)->next = tmp;
+	write(1, "sa\n", 3);
 }
 
 
 int main(void)
 {
-	ps_lst *stack_a;
-	ps_lst *stack_b;
-
 	// Stack A
 	ps_lst *stack_a_1 = malloc(sizeof(ps_lst));
 	ps_lst *stack_a_2 = malloc(sizeof(ps_lst));
@@ -39,6 +40,19 @@ int main(void)
 		printf("\e[42m STACK A \e[49m\n");
 	else 
 		printf("\e[101m STACK A \e[49m\n");
-	
+	// Stack A Data(Copy the position for us to check)
+	int stack_a_data_1 = stack_a_1->num;
+	int stack_a_data_2 = stack_a_2->num;
+	// printf("\nValue Stack A(P1):%d, (P2): %d\n", stack_a_1->num, stack_a_2->num);
+	// if(stack_a_data_1 == stack_a_2->num && stack_a_data_2 == stack_a_1->num) 
+	// 	printf("\n\e[42m EXCHANGE SUCCESSFULLY COMPLETED A \e[49m\n");
+	// else
+	// 	printf("\e[101m FAULTY EXCHANGE \e[49m\n");
+	sa(&stack_a_1);
+	printf("\nValue Stack A(P1):%d, (P2): %d\n", stack_a_1->num, stack_a_1->next->num);
+	if(stack_a_data_1 == stack_a_1->next->num && stack_a_data_2 == stack_a_1->num) 
+		printf("\n\e[42m EXCHANGE SUCCESSFULLY COMPLETED A \e[49m\n");
+	else
+		printf("\e[101m FAULTY EXCHANGE \e[49m\n");
 	return (0);
 }
