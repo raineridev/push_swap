c841fbe90daf95ba19439b325e9d7ac85034fa8c
Marcelo RAineri <raineridev@gmail.com>
2024-10-01 16:55:31 +0100

    update:implement double linked link

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:59:45 by mraineri          #+#    #+#             */
-/*   Updated: 2024/10/01 12:59:46 by mraineri         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:10:13 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef	struct p_lst
 {
 	int	num;
 	struct p_lst *next;
+	struct p_lst *prev;
 } ps_lst;
 
 #endif
