fe6bc5b056ffbafb651003357a331149936aed33
Marcelo Raineri <marcelo@gutlz.com>
2024-10-25 14:25:03 +0100

    del:remove main

--- push_swap.c
+++ push_swap.c
@@ -3,16 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: mraineri <mraineri@studenbt.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:58:13 by mraineri          #+#    #+#             */
-/*   Updated: 2024/10/02 15:22:36 by mraineri         ###   ########.fr       */
+/*   Updated: 2024/10/24 21:51:56 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int main(int argc, char *argv[])
-{
-    return (0);
-}
