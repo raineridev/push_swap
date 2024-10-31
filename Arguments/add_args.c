fd47243e445990f12ec5b391392a7f56e18f34e2
Marcelo RAineri <raineridev@gmail.com>
2024-10-31 13:39:40 +0000

    fix:leaks :)

--- Arguments/add_args.c
+++ Arguments/add_args.c
@@ -6,24 +6,27 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:23:33 by mraineri          #+#    #+#             */
-/*   Updated: 2024/10/07 15:04:41 by mraineri         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:16:13 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void add_args(ps_lst **head, int *args)
+void add_args(ps_lst **head, int *args, int size)
 {
     ps_lst *node;
-    int i;
-    if(!args)
-        return ;
-    i = 0;
+    int i = 0;
+
+    if (!args || !args[i])
+        return;
+
     *head = ps_create(args[i]);
-    while(args[i++])
+    
+    while (i <= size)
     {
-        node = ps_create(args[i]);
-        ps_link(head ,&node, i);
+        node = ps_create(args[i++]);
+        ps_link(head, &node, i);
     }
 }
+
 
