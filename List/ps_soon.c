56c8330a0efb4fbead4b5c7c9b0edbfd095680dc
Marcelo Raineri <marcelo@gutlz.com>
2024-11-06 19:26:34 +0000

    add:find the nearest number function

---
+++ List/ps_soon.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_soon.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mraineri <mraineri@studenbt.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 19:59:08 by mraineri          #+#    #+#             */
+/*   Updated: 2024/11/06 15:44:59 by mraineri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.c"
+#include "limits.h"
+
+ps_lst *ps_soon(ps_lst *stack_a, ps_lst *stack_b) 
+{
+    ps_lst *cheap_node;
+    ps_lst *tmp_b;
+    
+    if(!stack_a || !stack_b)
+        return (NULL);
+    tmp_b = stack_b;
+    cheap_node = NULL;
+    while(tmp_b)
+    {
+        if(stack_a->num > tmp_b->num)
+        {
+            if (!cheap_node)
+                cheap_node = tmp_b;
+            if (cheap_node->num < tmp_b->num)
+                cheap_node = tmp_b;
+        }
+        tmp_b = tmp_b->next;
+    }
+    if(!cheap_node)
+        return (ps_bigest(stack_b));
+    return (cheap_node);
+}
