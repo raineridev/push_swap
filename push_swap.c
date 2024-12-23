6632c21284036772d9c83ef8a8f25383e09521b2
Marcelo Raineri <raineridev@gmail.com>
2024-12-23 17:32:00 +0000

    fix:norm

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:58:13 by mraineri          #+#    #+#             */
-/*   Updated: 2024/12/23 16:28:50 by mraineri         ###   ########.fr       */
+/*   Updated: 2024/12/23 17:31:44 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 int	main(int argv, char *argc[])
 {
-	int *args = trim(argv, argc);
-	if (argv  < 3)
+	t_lst		*stack_a;
+	t_lst		*stack_b;
+	int			*args;
+
+	args = trim(argv, argc);
+	if (argv < 3)
 	{
 		write(2, "Error\n", 6);
 		free(args);
 		exit(1);
 	}
 	validate_args(argv, args);
-	t_lst	*stack_a = NULL;
-	t_lst	*stack_b = NULL;
-	add_args(&stack_a, args,(argv - 1));
+	stack_a = NULL;
+	stack_b = NULL;
+	add_args(&stack_a, args, (argv - 1));
 	free(args);
 	if (ps_size(stack_a) > 3)
 		moviment_stack_b(&stack_a, &stack_b);
